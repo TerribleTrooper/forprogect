76456bc24b03509a0aa954d0ceadb05aa8804624
TerribleTrooper <danilintima@icloud.com>
2023-06-28 20:28:05 +0300

    rt

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
