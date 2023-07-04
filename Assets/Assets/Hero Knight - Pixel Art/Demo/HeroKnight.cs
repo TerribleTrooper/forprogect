2651662f6e8f945a05b22cec552d7cf1e2dcbc54
TerribleTrooper <danilintima@icloud.com>
2023-07-04 14:46:18 +0300

    ew

--- Assets/Assets/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/Assets/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class HeroKnight : MonoBehaviour 
+public class HeroKnight : MonoBehaviour
 {
 
     [Header("Для выносливости")]
@@ -12,38 +12,37 @@ public class HeroKnight : MonoBehaviour
     private bool m_isShield = false; // Проверка с щитом персонаж
 
     [Header("Для ударов")]
-    [SerializeField] private Transform m_attackPoint;
+    [SerializeField] private Transform[] m_attackPoints;
     [SerializeField] private float m_attackRange = 0.5f;
     [SerializeField] private LayerMask m_enemyLayers;
     [SerializeField] private float m_attackDamage = 20;
 
     [Header("Для персонажа")]
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
-    [SerializeField] float      m_rollForce = 6.0f;
-    [SerializeField] bool       m_noBlood = false;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
+    [SerializeField] float m_rollForce = 6.0f;
+    [SerializeField] bool m_noBlood = false;
     [SerializeField] GameObject m_slideDust;
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_HeroKnight   m_groundSensor;
-    private Sensor_HeroKnight   m_wallSensorR1;
-    private Sensor_HeroKnight   m_wallSensorR2;
-    private Sensor_HeroKnight   m_wallSensorL1;
-    private Sensor_HeroKnight   m_wallSensorL2;
-    private bool                m_isWallSliding = false;
-    private bool                m_grounded = false;
-    private bool                m_rolling = false;
-    private int                 m_facingDirection = 1;
-    private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
-    private float               m_delayToIdle = 0.0f;
-    private float               m_rollDuration = 8.0f / 14.0f;
-    private float               m_rollCurrentTime;
-
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_HeroKnight m_groundSensor;
+    private Sensor_HeroKnight m_wallSensorR1;
+    private Sensor_HeroKnight m_wallSensorR2;
+    private Sensor_HeroKnight m_wallSensorL1;
+    private Sensor_HeroKnight m_wallSensorL2;
+    private bool m_isWallSliding = false;
+    private bool m_grounded = false;
+    private bool m_rolling = false;
+    private int m_facingDirection = 1;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private float m_delayToIdle = 0.0f;
+    private float m_rollDuration = 8.0f / 14.0f;
+    private float m_rollCurrentTime;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
@@ -54,17 +53,17 @@ public class HeroKnight : MonoBehaviour
         m_wallSensorL2 = transform.Find("WallSensor_L2").GetComponent<Sensor_HeroKnight>();
     }
 
-    void Update ()
+    void Update()
     {
         // Increase timer that controls attack combo
         m_timeSinceAttack += Time.deltaTime;
 
         // Increase timer that checks roll duration
-        if(m_rolling)
+        if (m_rolling)
             m_rollCurrentTime += Time.deltaTime;
 
         // Disable rolling if timer extends duration
-        if(m_rollCurrentTime > m_rollDuration)
+        if (m_rollCurrentTime > m_rollDuration)
             m_rolling = false;
 
         //Check if character just landed on the ground
@@ -91,15 +90,13 @@ public class HeroKnight : MonoBehaviour
         }
 
         // Swap direction of sprite depending on walk direction
-       
-
         if (inputX > 0)
         {
             GetComponent<SpriteRenderer>().flipX = false;
             m_facingDirection = 1;
-            
+
         }
-            
+
         else if (inputX < 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
@@ -111,8 +108,7 @@ public class HeroKnight : MonoBehaviour
         {
             m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
             m_stamina -= 1f * Time.deltaTime;
-        }    
-            
+        }
 
         //Set AirSpeed in animator
         m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
@@ -130,7 +126,7 @@ public class HeroKnight : MonoBehaviour
         }
 
         //Attack
-        else if(Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling && m_stamina >= 5f)
+        else if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling && m_stamina >= 5f)
         {
             Attack();
             m_currentAttack++;
@@ -174,7 +170,7 @@ public class HeroKnight : MonoBehaviour
             m_animator.SetTrigger("Roll");
             m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
             m_stamina -= 10f;
-        }   
+        }
 
         //Jump
         else if (Input.GetKeyDown("space") && m_grounded && !m_rolling && m_stamina >= 10f)
@@ -200,8 +196,8 @@ public class HeroKnight : MonoBehaviour
         {
             // Prevents flickering transitions to idle
             m_delayToIdle -= Time.deltaTime;
-                if(m_delayToIdle < 0)
-                    m_animator.SetInteger("AnimState", 0);
+            if (m_delayToIdle < 0)
+                m_animator.SetInteger("AnimState", 0);
         }
         // For Stamina
         ControleStamina();
@@ -232,7 +228,7 @@ public class HeroKnight : MonoBehaviour
         if (m_stamina > 100f)
             m_stamina = 100f;
 
-        else if(m_stamina < 0f)
+        else if (m_stamina < 0f)
             m_stamina = 0f;
 
         m_staminaBar.value = m_stamina;
@@ -240,19 +236,25 @@ public class HeroKnight : MonoBehaviour
 
     void Attack()
     {
-        Collider2D[] _hitEnemies = Physics2D.OverlapCircleAll(m_attackPoint.position, m_attackRange, m_enemyLayers);
-
-        foreach (Collider2D enemy in _hitEnemies)
+        foreach (Transform _attackPoint in m_attackPoints)
         {
-            enemy.GetComponent<EnemyScript>().TakeDamage(m_attackDamage);
+            Collider2D[] _hitEnemies = Physics2D.OverlapCircleAll(_attackPoint.position, m_attackRange, m_enemyLayers);
+            foreach (Collider2D enemy in _hitEnemies)
+            {
+                enemy.GetComponent<EnemyScript>().TakeDamage(m_attackDamage);
+            }
         }
+
     }
 
     void OnDrawGizmosSelected()
     {
-        if (m_attackPoint == null)
-            return;
+        foreach (Transform _attackPoint in m_attackPoints)
+        {
+            if (m_attackPoints == null)
+                return;
+            Gizmos.DrawWireSphere(_attackPoint.position, m_attackRange);
+        }
 
-        Gizmos.DrawWireSphere(m_attackPoint.position, m_attackRange);
     }
 }
